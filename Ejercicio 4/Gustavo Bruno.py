f5b96e80cf152bda5d5668551595b6b32bfd80e6
geb2701 <gus53@live.com.ar>
2023-08-15 20:00:01 -0300

    Archivo 4

---
+++ Ejercicio 4/Gustavo Bruno.py
@@ -0,0 +1,30 @@
+def calcularVuelto (t,d, tipoBilletes):
+    vuelto=d-t
+    vueltos = []
+    if (vuelto > 0):
+        
+
+        for i in range(len(tipoBilletes)):
+            vueltos.append(vuelto // tipoBilletes[i])
+            vuelto = vuelto % tipoBilletes[i]
+
+        for i in range(len(tipoBilletes)):
+            if vueltos[i]>0:
+                print("Billetes de " + str(tipoBilletes[i]) + ": " + str(vueltos[i]))
+    elif(vuelto == 0):
+        print("No hay que dar Vuelto")
+    else:
+        print("Aun le faltan $" + str(vuelto * - 1))
+
+   
+total=int(input("Ingresar el valor total de la compra: "))
+dinero=int(input("Ingresar el valor de la compra:"))
+tipoBilletes = [2000,1000,500,100,50,20,10,5,1]
+
+if (total <= dinero):
+    vueltos = calcularVuelto (total,dinero, tipoBilletes)
+    for i in range(len(tipoBilletes)):
+        if vueltos[i]>0:
+            print("Billetes de " + str(tipoBilletes[i]) + ": " + str(vueltos[i]))
+else:
+    print(" El dinero suministrado es menor que el valor de la compra")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ALGORITMOS Y ESTRUCTURAS DE DATOS I
- 
+
+Nombrar Carpetas: Ejercicio + Numero
+Nombre Archivos: Nombre y Apellido
+
+Por favor no modificar los archivos de otras personas
+
+Ante error avisen
